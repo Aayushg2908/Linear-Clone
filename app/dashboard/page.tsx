abc67d515c084412c7e3c11e6cd738e81623acaf
Aayush <aayushg2908@gmail.com>
2024-05-27 16:04:43 +0530

    fix: deployment issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@/auth";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 
-const DashboardPage = async ({ children }: { children: React.ReactNode }) => {
+const DashboardPage = async () => {
   const session = await auth();
   if (!session?.user && !session?.user?.id) {
     return redirect("/sign-in");
