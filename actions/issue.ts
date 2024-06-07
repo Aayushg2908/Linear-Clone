776e051b99a8bbdafb4fa741a6c51bac542e7d53
Aayush <aayushg2908@gmail.com>
2024-06-07 19:14:00 +0530

    feat: change status of issue through dropdown

--- actions/issue.ts
+++ actions/issue.ts
@@ -2,7 +2,7 @@
 
 import { auth } from "@/auth";
 import { db } from "@/lib/db";
-import { ISSUETYPE } from "@prisma/client";
+import { ISSUELABEL, ISSUETYPE } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
@@ -64,4 +64,67 @@ export const getAllIssues = async ({
       order: "asc",
     },
   });
+};
+
+export const updateIssue = async ({
+  id,
+  workspaceId,
+  userId,
+  values,
+}: {
+  id: string;
+  workspaceId: string;
+  userId: string;
+  values: {
+    title?: string;
+    content?: string;
+    status?: ISSUETYPE;
+    order?: number;
+    label?: ISSUELABEL;
+  };
+}) => {
+  const session = await auth();
+  if (!session?.user && !session?.user?.id) {
+    return redirect("/sign-in");
+  }
+
+  const issue = await db.issue.findUnique({
+    where: {
+      id,
+      workspaceId,
+    },
+    include: {
+      assignedTo: true,
+    },
+  });
+  if (!issue) {
+    return { error: "Issue not found" };
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+  if (!user) {
+    return { error: "User not found" };
+  }
+
+  if (issue.ownerId === session.user.id || issue.assignedTo.includes(user)) {
+    await db.issue.update({
+      where: {
+        id,
+        workspaceId,
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    revalidatePath(`/dashboard/${workspaceId}`);
+
+    return { success: "Issue status updated successfully!" };
+  }
+
+  return { error: "You do not have the permission to do this action!" };
 };
--- app/dashboard/[workspaceId]/issue-card.tsx
+++ app/dashboard/[workspaceId]/issue-card.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { updateIssue } from "@/actions/issue";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Issues } from "@/constants";
 import { useCreateIssue } from "@/hooks/use-create-issue";
 import { cn } from "@/lib/utils";
-import { Issue } from "@prisma/client";
-import { Grip, PlusIcon } from "lucide-react";
+import { ISSUETYPE, Issue } from "@prisma/client";
+import { CheckIcon, Grip, PlusIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
 
 interface IssueCardProps {
   issues: Issue[];
@@ -13,6 +24,34 @@ interface IssueCardProps {
 
 export const IssueCard = ({ issues }: IssueCardProps) => {
   const { onOpen } = useCreateIssue();
+  const { data } = useSession();
+  const params = useParams();
+  const [statusLoading, setStatusLoading] = useState(false);
+
+  const handleStatusSelect = async (id: string, status: ISSUETYPE) => {
+    try {
+      setStatusLoading(true);
+      toast.loading("Updating issue status...");
+      const response = await updateIssue({
+        id,
+        userId: data?.user?.id!,
+        workspaceId: params.workspaceId as string,
+        values: {
+          status,
+        },
+      });
+      if (response.error) {
+        toast.error(response.error);
+      } else if (response.success) {
+        toast.success(response.success);
+      }
+    } catch (error) {
+      toast.error("Something went wrong! Please try again.");
+    } finally {
+      setStatusLoading(false);
+      toast.dismiss();
+    }
+  };
 
   return (
     <div className="mt-4 w-full flex flex-col sm:flex-row gap-4 sm:flex-wrap p-2 justify-center">
@@ -49,23 +88,54 @@ export const IssueCard = ({ issues }: IssueCardProps) => {
                     key={iss.id}
                     className="flex justify-between items-center border border-neutral-700 rounded-lg p-2"
                   >
-                    <span className="flex gap-x-1 items-center">
-                      <issue.Icon
-                        className={cn(
-                          "size-5 text-white rounded-full",
-                          issue.type === "DONE" && "bg-green-600 ",
-                          issue.type === "INPROGRESS" && "bg-yellow-600",
-                          issue.type === "CANCELLED" && "bg-red-600"
-                        )}
-                      />
-                      {iss.title}
+                    <span className="flex gap-x-1 items-center line-clamp-1">
+                      <DropdownMenu>
+                        <DropdownMenuTrigger>
+                          <issue.Icon
+                            className={cn(
+                              "size-4 text-white rounded-full",
+                              issue.type === "DONE" && "bg-green-600 ",
+                              issue.type === "INPROGRESS" && "bg-yellow-600",
+                              issue.type === "CANCELLED" && "bg-red-600"
+                            )}
+                          />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                          {Issues.map((issue) => (
+                            <DropdownMenuItem
+                              key={issue.type}
+                              className="flex items-center justify-between cursor-pointer"
+                              onSelect={() =>
+                                handleStatusSelect(iss.id, issue.type)
+                              }
+                            >
+                              <span className="flex gap-x-2 items-center">
+                                <issue.Icon
+                                  className={cn(
+                                    "size-4 text-white rounded-full",
+                                    issue.type === "DONE" && "bg-green-600 ",
+                                    issue.type === "INPROGRESS" &&
+                                      "bg-yellow-600",
+                                    issue.type === "CANCELLED" && "bg-red-600"
+                                  )}
+                                />
+                                {issue.name}
+                              </span>
+                              {issue.type === iss.status && (
+                                <CheckIcon className="ml-1 size-4 text-green-600" />
+                              )}
+                            </DropdownMenuItem>
+                          ))}
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                      <span className="text-sm">{iss.title}</span>
                     </span>
                     <Button
                       size="icon"
                       variant="ghost"
                       className="border border-neutral-700"
                     >
-                      <Grip className="size-5" />
+                      <Grip className="size-4" />
                     </Button>
                   </div>
                 )}
