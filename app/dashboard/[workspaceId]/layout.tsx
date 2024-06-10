ec29eb7352265b914e4113e2d1803bb2ad9da2a8
Aayush <aayushg2908@gmail.com>
2024-06-10 12:08:27 +0530

    fix: protecting each workspace from users that are not members in that workspace

--- app/dashboard/[workspaceId]/layout.tsx
+++ app/dashboard/[workspaceId]/layout.tsx
@@ -1,4 +1,7 @@
+import { db } from "@/lib/db";
 import { Sidebar } from "./sidebar";
+import { notFound, redirect } from "next/navigation";
+import { auth } from "@/auth";
 
 const WorkspaceLayout = async ({
   children,
@@ -7,6 +10,27 @@ const WorkspaceLayout = async ({
   children: React.ReactNode;
   params: { workspaceId: string };
 }) => {
+  const session = await auth();
+  if (!session?.user && session?.user?.id) {
+    return redirect("/sign-in");
+  }
+
+  const workspace = await db.workspace.findUnique({
+    where: {
+      id: params.workspaceId,
+    },
+    select: {
+      members: true,
+    },
+  });
+  if (!workspace) {
+    return notFound();
+  }
+
+  if (!workspace.members.find((member) => member.id === session?.user?.id)) {
+    return redirect("/dashboard/join");
+  }
+
   return (
     <div className="w-full h-full flex">
       <div className="max-md:hidden">
